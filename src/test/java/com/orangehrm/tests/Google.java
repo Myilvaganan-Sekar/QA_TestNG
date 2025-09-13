11eaf7632c70d900d066612c9eb5ad3a7a56cafa
QATester <test@gmail.com>
2025-09-13 12:49:58 +0530

    Updated the google script

--- src/test/java/com/orangehrm/tests/Google.java
+++ src/test/java/com/orangehrm/tests/Google.java
@@ -3,5 +3,6 @@ package com.orangehrm.tests;
 public class Google {
 	public static void main(String[] args) {
 		System.out.println("Launch Google webPage");
+		System.out.println("Succesfully Launched");
 	}
 }
